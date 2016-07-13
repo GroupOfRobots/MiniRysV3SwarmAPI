42daa27141b581e388d3cb959588a32fe33bcc23
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-07-13 22:36:44 +0200

    [ADD] sketch of sockets communication

--- include/api.h
+++ include/api.h
@@ -1,16 +1,46 @@
 #ifndef API_H
 #define API_H
 
+#include <QObject>
+#include <QMap>
+#include <QWebSocket>
+#include <QWebSocketServer>
 #include <iostream>
 
 namespace irys {
 
-class API {
+class API : protected QObject {
+    Q_OBJECT
 public:
     API();
     virtual ~API();
 
-    void foo() const;
+    /// Sends byte-array to the specific robot with passed id.
+    /// Throws DataTransferToRobotUnsuccessful.
+    void sendDataToRobotWithId(int id, QByteArray data);
+
+    /// Sends byte-array to each robot.
+    /// Throws DataTransferToRobotUnsuccessful.
+    void sendDataToEachRobot(QByteArray data);
+
+Q_SIGNALS:
+    /// Signal emited after websocket server closure.
+    void serverClosed();
+
+    /// Signal emited after disconnection a robot with specific id.
+    void robotDisconnected(int id);
+
+private Q_SLOTS:
+    void onNewConnection();
+    void processReceivedData(QByteArray data);
+    void robotSocketDisconnected();
+
+private:
+    // wsockets side should be separated!
+    QWebSocket m_webSocket;
+    QWebSocketServer *m_Server;
+    // websocket assigned to robot's id
+    QMap<int, QWebSocket *> m_robotsSockets;
 };
 
 }
--- src/api.cpp
+++ src/api.cpp
@@ -1,11 +1,10 @@
 #include "api.h"
 
-irys::API::API() {
+irys::API::API()
+    : m_Server(new QWebSocketServer(
+                   QStringLiteral("Server name"),
+                   QWebSocketServer::SecureMode, this)) {
 
 }
 
 irys::API::~API() { }
-
-void irys::API::foo() const {
-    std::cout << "Hello from foo!\n";
-}
