a145e9ed66eb1e4eb7878ccb7a6ff688a0d98323
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-07-17 21:41:32 +0200

    [ADD] dbus exceptions

--- include/exceptions.h
+++ include/exceptions.h
@@ -5,19 +5,30 @@
 
 namespace irys {
 
-class Exception : public std::exception {
-    Exception(const std::string& whatArg) : std::exception(whatArg)
-    { }
-};
-
 class RuntimeError : public std::runtime_error {
+public:
     RuntimeError(const std::string& whatArg) : std::runtime_error(whatArg)
     { }
 };
 
 class DataTransferToRobotUnsuccessful : public RuntimeError {
+public:
     DataTransferToRobotUnsuccessful(int id)
         : RuntimeError("Unsuccessful data transfer to a robot with id: ")
+    { }
+};
+
+class GetAllDevicesUnsuccessful : public RuntimeError {
+public:
+    GetAllDevicesUnsuccessful()
+        : RuntimeError("Unable to get devices list from NM interface")
+    { }
+};
+
+class GetDeviceTypeUnsuccessful : public RuntimeError {
+public:
+    GetDeviceTypeUnsuccessful()
+        : RuntimeError("Unable to get device type")
     { }
 };
 
