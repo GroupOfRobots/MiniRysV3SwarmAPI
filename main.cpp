02e8a16809cdc615a9f823e85ead2f417f8e5276
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-12-19 22:51:35 +0100

    [ADD] cleaner main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include "robotstatus.h"
-#include "wsclient.h"
-//#include "wsserver.h"
 #include <iostream>
 #include <thread>
 #include <future>
 #include "communicator.h"
 
 int main(int argc, char *argv[]) {
-    //irys::api::WSClient client;
     irys::api::RobotStatus rs;
     rs.id = 12;
+
     irys::api::Communicator c;
-    //auto handle = std::async(std::launch::async, &irys::api::WSClient::dispatch, &client);
-    //std::thread t (&irys::api::WSClient::dispatch, &client);
-    //client.processReceivedRobotStatus();
-    //client.dispatch();
     for (int i=0; i < 10; ++i)
         c.send(rs);
 }
